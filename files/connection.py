2ed2216a1da6651dfc632825f0a5bde3c0719d7e
OMISTAJA <sofia.jarvela@metropolia.fi>
2024-02-26 14:25:34 +0200

    Tässä vähän alkua

---
+++ files/connection.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+connect = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    database="flight_game",
+    user="root",
+    password="root",
+    autocommit=True
+)
