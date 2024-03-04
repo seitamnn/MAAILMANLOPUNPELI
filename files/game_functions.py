747c1228f614e1c0c4bb2204206793c360fb9ace
OMISTAJA <sofia.jarvela@metropolia.fi>
2024-03-04 13:31:54 +0200

    miu

--- files/game_functions.py
+++ files/game_functions.py
@@ -38,14 +38,26 @@ def select_airport():
 chosen_airport = select_airport()
 print("have a safe flight to", chosen_airport + "!")
 
-def challenge_currency_add(add_amount):
+def challenge_currency_add(add_amount, screen_name):
     currency_sql = f"UPDATE game SET currency += '{add_amount}'"
     cursor = connect.cursor()
     cursor.execute(currency_sql)
     reuslt = cursor.fetchall()
 
-def challenge_currency_subtract(subtract_amount):
-    currency_sql = f"UPDATE game SET currency -= '{subtract_amount}'"
+def challenge_currency_subtract(subtract_amount, screen_name):
+    currency_sql = f"UPDATE game SET currency -= '{subtract_amount}' "
     cursor = connect.cursor()
     cursor.execute(currency_sql)
+    reuslt = cursor.fetchall()
+
+def challenge_distance_add(add_amount, screen_name):
+    distance_sql = "UPDATE game SET alien_distance = alien_distance += {add_amount} WHERE screen_name = '{screen_name}'"
+    cursor = connect.cursor()
+    cursor.execute(distance_sql)
+    reuslt = cursor.fetchall()
+
+def challenge_distance_substract(subtract_amount, screen_name):
+    distance_sql = "UPDATE game SET alien_distance = alien_distance += {add_amount} WHERE screen_name = '{screen_name}'"
+    cursor = connect.cursor()
+    cursor.execute(distance_sql)
     reuslt = cursor.fetchall()
